7e50fc5065d1ead024c6cde0bdf14c073fa21218
taanyaahp <taanyaahp@gmail.com>
2026-01-27 21:57:27 -0800

    organized files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
-import Nav from './Nav';
-import Header from './Header';
-import Main from './Main';
-import Footer from './Footer';
+import Nav from './main-components/Nav.js';
+import Header from './main-components/Header.js';
+import Main from './main-components/Main.js';
+import Footer from './main-components/Footer.js';
 import { Routes, Route } from 'react-router-dom';
-import Homepage from './Homepage';
-import BookingPage from './BookingPage.js'
+import Homepage from './components/Homepage.js';
+import BookingPage from './components/BookingPage.js'
 
 function App() {
   return (
