ee638226b357c510e01bea236aa53568a942ccb1
taanyaahp <taanyaahp@gmail.com>
2026-02-06 21:56:38 -0800

    made navbar and booking form responsive

--- src/main-components/Nav.js
+++ src/main-components/Nav.js
@@ -1,14 +1,18 @@
-import logo from '../images/navLogo.png';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+import logo from '../images/navLogo.png';
+import hamburgerIcon from '../images/hamburgerIcon.png';
 import '../App.css';
 
 function Nav() {
+    const [isOpen, setIsOpen] = useState(false);
+
     return(
         <nav className="navigation">
             <Link className="logo-container" to="/">
                 <img className="logo" src={logo} alt="little lemon logo"/>
             </Link>
-            <ul className="nav-link-container">
+            <ul className={`nav-link-container${isOpen ? "-open" : ""}`}>
                 <li className="nav-link"><Link to="/">Home</Link></li>
                 <li className="nav-link"><a href="/#">About</a></li>
                 <li className="nav-link"><a href="/#">Menu</a></li>
@@ -16,6 +20,9 @@ function Nav() {
                 <li className="nav-link"><a href="/#">Order Online</a></li>
                 <li className="nav-link"><a href="/#">Login</a></li>
             </ul>
+            <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+                <img  className="hamburger-icon" src={hamburgerIcon} alt="navigation menu icon"/>
+            </div>
         </nav>
     );
 }
