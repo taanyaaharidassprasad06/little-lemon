272b000a959dff67482635e3da02426c2552f0be
taanyaahp <taanyaahp@gmail.com>
2026-02-07 22:09:40 -0800

    final version of little lemon capstone project

--- src/components/ConfirmedBooking.js
+++ src/components/ConfirmedBooking.js
@@ -6,24 +6,24 @@ function ConfirmedBooking() {
 
 
     return (
-        <div>
-            <h1>Your Reservation Has Been Confirmed</h1>
-            <h4>
-                Thank you! Your reservation has been confirmed for {booking.date} at {booking.time} 
+        <div className="confirmation-container">
+            <h1 className="confirmation-header">Your Reservation Has Been Confirmed</h1>
+            <h4 className="confirmation-message">
+                Thank you! Your reservation has been confirmed for {booking.date} at {booking.selectedTime} 
             </h4>
-            <p>A confirmation has been sent to {booking.email}. Please print or save the reservation from your email.</p>
+            <p className="confirm">A confirmation has been sent to {booking.email}. Please print or save the reservation from your email.</p>
 
-            <div>
+            <div className="receipt">
                 <h3>Reservation Details:</h3>
                 <p><strong>{booking.name}'s party of {booking.guests}</strong></p>
                 <p>Date: {booking.date}</p>
-                <p>Time: {booking.time}</p>
+                <p>Time: {booking.selectedTime}</p>
                 <p>Occasion: {booking.occasion}</p>
                 <p>Preferred Seating: {booking.seat}</p>
             </div>
 
             <Link to="/">
-                <button aria-label="On Click">Return to Home</button>
+                <button className="home-btn" aria-label="On Click">Return to Home</button>
             </Link>
         </div>
     );
